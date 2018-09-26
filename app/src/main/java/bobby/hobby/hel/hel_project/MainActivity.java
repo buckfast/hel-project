67c6e21fb108bb716cbad9d3e393f3fa33b9ac77
unknown <lyhoang080696@gmail.com>
2018-09-26 13:34:06 +0300

    initial commit

---
+++ app/src/main/java/bobby/hobby/hel/hel_project/MainActivity.java
@@ -0,0 +1,13 @@
+package bobby.hobby.hel.hel_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
