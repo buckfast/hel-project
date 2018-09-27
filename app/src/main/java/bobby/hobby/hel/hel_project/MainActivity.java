284e232af15289d6b7c88de06f5aab13542be0c9
unknown <lyhoang080696@gmail.com>
2018-09-27 13:38:51 +0300

    add View Model and Live data

--- app/src/main/java/bobby/hobby/hel/hel_project/MainActivity.java
+++ app/src/main/java/bobby/hobby/hel/hel_project/MainActivity.java
@@ -1,13 +1,21 @@
 package bobby.hobby.hel.hel_project;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, notes -> {
+            Toast.makeText(this, "onChanged", Toast.LENGTH_SHORT).show();
+        });
     }
 }
