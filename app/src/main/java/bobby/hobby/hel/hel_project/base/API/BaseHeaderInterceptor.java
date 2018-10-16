437377c6d69c90b683fc5997e4bd74b863e411cb
unknown <lyhoang080696@gmail.com>
2018-10-16 11:42:39 +0300

    oops, did not add that time

--- app/src/main/java/bobby/hobby/hel/hel_project/base/API/BaseHeaderInterceptor.java
+++ app/src/main/java/bobby/hobby/hel/hel_project/base/API/BaseHeaderInterceptor.java
@@ -1,4 +1,37 @@
 package bobby.hobby.hel.hel_project.base.API;
 
-public class BaseHeaderInterceptor {
+
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public abstract class BaseHeaderInterceptor implements Interceptor {
+    protected abstract Map<String, String> returnHeaderParams();
+    @Override
+    public Response intercept(Chain chain) {
+        Request originalRequest = chain.request();
+        Map<String, String> params = returnHeaderParams();
+        Request.Builder builder = originalRequest.newBuilder();
+
+        if (params != null) {
+            for(Map.Entry<String, String> param: params.entrySet()) {
+                builder.addHeader(param.getKey(), param.getValue());
+            }
+        }
+
+        Request newRequest = builder.build();
+
+        try {
+            return chain.proceed(newRequest);
+        } catch (IOException e) {
+            Log.e("Header", "Error building header with interceptor");
+            return null;
+        }
+    }
 }
