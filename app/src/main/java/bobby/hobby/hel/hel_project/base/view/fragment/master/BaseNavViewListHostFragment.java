644040fc320b3e9ccc600e41adcc6d6f916219cd
unknown <lyhoang080696@gmail.com>
2018-10-18 14:07:49 +0300

    sorry my bad, should have done this before, this error cause by not properly test

--- app/src/main/java/bobby/hobby/hel/hel_project/base/view/fragment/master/BaseNavViewListHostFragment.java
+++ app/src/main/java/bobby/hobby/hel/hel_project/base/view/fragment/master/BaseNavViewListHostFragment.java
@@ -21,6 +21,9 @@ public abstract class BaseNavViewListHostFragment<T extends BaseViewModel, V ext
     protected abstract BaseNavViewListChildFragment returnLeftChild();
     protected abstract BaseNavViewListChildFragment returnRightChild();
     protected abstract int returnDrawerHostLayout();
+    protected abstract int returnGuidelineId();
+    protected abstract int returnLeftChildId();
+    protected abstract int returnRightChildId();
 
     protected float getGuidelineSplitPercentage () {
         return 0.3f;
@@ -35,12 +38,12 @@ public abstract class BaseNavViewListHostFragment<T extends BaseViewModel, V ext
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Guideline guideline = view.findViewById(R.id.guideline);
+        Guideline guideline = view.findViewById(returnGuidelineId());
         guideline.setGuidelinePercent(getGuidelineSplitPercentage());
         if (returnLeftChild() != null && returnRightChild() != null) {
             Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.left_child_drawer, returnLeftChild())
-                    .replace(R.id.right_child_drawer, returnRightChild())
+                    .replace(returnLeftChildId(), returnLeftChild())
+                    .replace(returnRightChildId(), returnRightChild())
                     .commit();
         }
         mFragmentsViewModel.getClickReaction().observe(this, v-> mViewModel.setClickReaction());
