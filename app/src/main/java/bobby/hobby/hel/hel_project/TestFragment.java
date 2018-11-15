a91b48a5387cf1dffa075a980b6e39a2b40cd3c5
unknown <lyhoang080696@gmail.com>
2018-11-15 11:18:17 +0200

    add progress bar and swipe to refresh

--- app/src/main/java/bobby/hobby/hel/hel_project/TestFragment.java
+++ app/src/main/java/bobby/hobby/hel/hel_project/TestFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,7 @@ import java.util.Objects;
 import bobby.hobby.hel.hel_project.base.view.fragment.BaseFragment;
 import bobby.hobby.hel.hel_project.repository.internal.model.User;
 
-public class TestFragment extends BaseFragment<ViewModelTest> implements BaseFragment.LongRunningTaskBehaviour{
+public class TestFragment extends BaseFragment<ViewModelTest> implements BaseFragment.LongRunningTaskBehaviour, BaseFragment.SwipeToRefreshBehaviour{
 
     @Nullable
     @Override
@@ -33,6 +35,21 @@ public class TestFragment extends BaseFragment<ViewModelTest> implements BaseFra
             String token = Objects.requireNonNull(user).getToken();
             Toast.makeText(getActivity(), token, Toast.LENGTH_SHORT).show();
         });
+        swipeRefreshLayout.setColorSchemeColors(ContextCompat.getColor(getActivity(),android.R.color.holo_blue_bright),
+                ContextCompat.getColor(getActivity(),android.R.color.holo_green_light),
+                ContextCompat.getColor(getActivity(),android.R.color.holo_orange_light),
+                ContextCompat.getColor(getActivity(),android.R.color.holo_red_light));
+
+    }
+
+    @Override
+    protected LongRunningTaskBehaviour returnLongRunningTaskBehaviour() {
+        return this;
+    }
+
+    @Override
+    protected SwipeToRefreshBehaviour returnSwipeToRefresh() {
+        return this;
     }
 
     @Override
@@ -48,5 +65,16 @@ public class TestFragment extends BaseFragment<ViewModelTest> implements BaseFra
     @Override
     public Fragment returnProgressBarFragment() {
         return new TestProgressBarFragment();
+    }
+
+    @Override
+    public int returnSwipeRefreshLayout() {
+        return R.id.refresh;
+    }
+
+    @Override
+    public void refresh(SwipeRefreshLayout layout) {
+        Toast.makeText(getActivity(), "Refreshed", Toast.LENGTH_SHORT).show();
+        layout.setRefreshing(false);
     }
 }
--- app/src/main/java/bobby/hobby/hel/hel_project/base/view/fragment/BaseFragment.java
+++ app/src/main/java/bobby/hobby/hel/hel_project/base/view/fragment/BaseFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,8 +24,18 @@ import bobby.hobby.hel.hel_project.base.viewmodel.BaseViewModel;
 
 public abstract class BaseFragment<T extends BaseViewModel> extends Fragment {
     protected T mViewModel;
+    protected LongRunningTaskBehaviour progressBehaviour;
+    protected SwipeRefreshLayout swipeRefreshLayout;
+    protected SwipeToRefreshBehaviour swipeToRefreshBehaviour;
 
     protected abstract Class<T> returnViewModel();
+    protected LongRunningTaskBehaviour returnLongRunningTaskBehaviour(){
+        return null;
+    }
+
+    protected SwipeToRefreshBehaviour returnSwipeToRefresh() {
+        return null;
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,25 +43,39 @@ public abstract class BaseFragment<T extends BaseViewModel> extends Fragment {
         if (returnViewModel() != null) {
             mViewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(returnViewModel());
         }
+        if (returnLongRunningTaskBehaviour() != null) {
+            progressBehaviour = returnLongRunningTaskBehaviour();
+        }
+        if (returnSwipeToRefresh() != null) {
+            swipeToRefreshBehaviour = returnSwipeToRefresh();
+        }
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if (this instanceof LongRunningTaskBehaviour) {
-            LongRunningTaskBehaviour behaviour = (LongRunningTaskBehaviour) this;
-            if (behaviour.returnProgressBarContainer() != -1 && behaviour.returnProgressBarFragment() != null) {
-                Fragment f = behaviour.returnProgressBarFragment();
+        if (progressBehaviour != null) {
+            if (progressBehaviour.returnProgressBarContainer() != -1 && progressBehaviour.returnProgressBarFragment() != null) {
+                Fragment f = progressBehaviour.returnProgressBarFragment();
                 getChildFragmentManager()
                         .beginTransaction()
-                        .replace(behaviour.returnProgressBarContainer(), f)
+                        .replace(progressBehaviour.returnProgressBarContainer(), f)
                         .commit();
             }
+        }
+        if (swipeToRefreshBehaviour != null) {
+            swipeRefreshLayout = view.findViewById(swipeToRefreshBehaviour.returnSwipeRefreshLayout());
+            swipeRefreshLayout.setOnRefreshListener(() -> swipeToRefreshBehaviour.refresh(swipeRefreshLayout));
         }
     }
 
     public interface LongRunningTaskBehaviour {
         int returnProgressBarContainer();
         Fragment returnProgressBarFragment();
+    }
+
+    public interface SwipeToRefreshBehaviour {
+        int returnSwipeRefreshLayout();
+        void refresh(SwipeRefreshLayout layout);
     }
 }
